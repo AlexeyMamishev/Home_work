95164cba8d9d4653d92c0f726b633f12dc861607
AlexeyMamishev <ntd2013@bk.ru>
2025-01-02 15:17:59 +0500

    Add files via upload

---
+++ module_1_9.py
@@ -0,0 +1,12 @@
+example = 'Практика'
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1::2])
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
