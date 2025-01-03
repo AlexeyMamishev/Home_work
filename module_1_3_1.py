940955a3c3f48b74e06418ded907e1a9d258e160
AlexeyMamishev <ntd2013@bk.ru>
2025-01-03 11:59:13 +0500

    Update and rename module_1_6.py to module_1_3_1.py

---
+++ module_1_3_1.py
@@ -0,0 +1,5 @@
+number_of_completed_HW = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_HW
+print('Курс:', course_name, 'всего задач:', number_of_completed_HW, 'затрачено часов:', 1.5, 'среднее время выполнения:', time_for_one_task)
